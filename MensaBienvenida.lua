3c473ede9bf3d8acda6ffb3957b8f766cb149428
Black7in <elperroblack158@gmail.com>
2025-07-30 14:31:56 -0400

    Enhance welcome message with additional support information and encouragement

--- MensaBienvenida.lua
+++ MensaBienvenida.lua
@@ -1,7 +1,10 @@
 
 
 local function enviarMensaje(eventid, delay, repeats, player)
-    local msg = "Bienvenido a Murloc Wow\n" .. player:GetName() .. "! Disfruta de tu aventura en Azeroth."
+    local msg = "Bienvenido a |cff33FF00Murloc Wow|r\n" .. player:GetName() .. "! Disfruta de tu aventura en Azeroth.\n" ..
+                "Si necesitas ayuda, puedes contactar a un Game Master o visitar nuestro sitio web.\n" ..
+                "Recuerda que estamos aquí para ayudarte a disfrutar de la mejor experiencia de juego posible.\n\n" ..
+                "¡Diviértete y buena suerte!\n"
     player:GossipClearMenu()
     player:GossipMenuAddItem(1, "", 0, 1, false, msg)
     player:GossipSendMenu(1, player, 0)
