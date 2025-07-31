b128a66ea796402b4232ca235d2038b2cc098227
Black7in <elperroblack158@gmail.com>
2025-07-30 20:30:30 -0400

    Refactor Discord login webhook to improve message formatting and logging

--- DiscordLogs/gm_login.lua
+++ DiscordLogs/gm_login.lua
@@ -1,9 +1,17 @@
 local function OnLogin(event, player)
     if player:GetGMRank() >= 1 then
-        local json = `{"content": `..player:GetName() ..` " se ha conectado al servidor."}`
-        HttpRequest("POST", "https://discord.com/api/webhooks/1400264964412801238/RdBNg8fXgkcTH5gY6YQGJvMJTbVE6-toZjuWzTFwS6R9xCvxUyQRAn0tvsBxrDuszbp", json, "application/json", function(status, body, headers)
-            print(body)
-        end)
+        local content = player:GetName() .. " se ha conectado al servidor."
+        local json = '{"content": "' .. content .. '"}'
+        
+        HttpRequest("POST",
+            "https://discord.com/api/webhooks/1400264964412801238/RdBNg8fXgkcTH5gY6YQGJvMJTbVE6-toZjuWzTFwS6R9xCvxUyQRAn0tvsBxrDuszbp",
+            json,
+            "application/json",
+            function(status, body, headers)
+                print("Webhook enviado. Estado: " .. status)
+            end
+        )
     end
 end
+
 RegisterPlayerEvent(3, OnLogin)
