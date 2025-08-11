6a0a827d4e268aa2b97445d7b2c1dcb6b0a0efca
Black7in <elperroblack158@gmail.com>
2025-08-11 10:42:03 -0400

    Expand initial areas for speed aura application and exclude GMs from receiving the aura

--- ZonasAreas/buffos_zona_inicial.lua
+++ ZonasAreas/buffos_zona_inicial.lua
@@ -1,17 +1,29 @@
 
-local areasInicio = {9, 363}
+local areasInicio = {9, 363, 24, 59, 34, 364, 365, 132, 800, 188, 256, 3526, 3560, 3528, 3529, 3559, 2117, 154, 155, 221, 220, 358, 3431, 3458}
 
 local doubleSpeed = 59737
 
 local function OnUpdateArea(event, player, oldArea, newArea)
+    if player:IsGM() then
+        return -- No aplicar aura a GMs
+    end
     -- verificar si el player esta en algun area de inicio
-    if newArea == 9 or newArea == 363 then
+    local isInStartArea = false
+    for _, area in ipairs(areasInicio) do
+        if newArea == area then
+            isInStartArea = true
+            break
+        end
+    end
+    -- si el player esta en un area de inicio, aplicar aura de velocidad
+    if isInStartArea then
         player:AddAura(doubleSpeed, player) -- aplicar aura de velocidad
         local aura = player:GetAura( doubleSpeed )
         if aura then
             -- 1 hora en milisegundos
             aura:SetDuration(3600000) -- 1 hora en milisegundos
         end
+        return
     else
         -- si sale de las areas de inicio, eliminar aura de velocidad
         if player:HasAura(doubleSpeed) then
