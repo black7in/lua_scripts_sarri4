4e44cdc25ae1d7fb950ab7b7b715f652c62a239d
Black7in <elperroblack158@gmail.com>
2025-08-15 00:23:28 -0400

    Add Gurky NPC with leveling challenge gossip options

---
+++ gurky_inscriptor_retos_leveo.lua
@@ -0,0 +1,17 @@
+local npc = 50016
+
+local text = "¡Hola! Soy Gurky, el de los retos de leveo. ¿Quieres elegir un reto de leveo? ¡Será divertido! ¡Vamos a ver qué tan rápido puedes subir de nivel!"
+
+local function OnGossipHello(event, player, object)
+    player:GossipClearMenu()
+    player:GossipMenuAddItem(0, "Alimentar", 0, 1)
+    player:GossipMenuAddItem(0, "Salir", 0, 2)
+    player:SendGossipText(text, npc)
+    player:GossipSendMenu(npc, object)
+end
+RegisterCreatureGossipEvent(npc, 1, OnGossipHello)
+
+local function OnGossipSelect(event, player, object, sender, intid, code, menu_id)
+
+end
+RegisterCreatureGossipEvent(npc, 2, OnGossipSelect)
