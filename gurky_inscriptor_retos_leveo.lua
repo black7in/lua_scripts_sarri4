371a6f83212b343d85ad16e33fa8cdb20d2e1503
Black7in <elperroblack158@gmail.com>
2025-08-15 00:30:37 -0400

    Add new gossip options for Gurky NPC to enhance player interaction

--- gurky_inscriptor_retos_leveo.lua
+++ gurky_inscriptor_retos_leveo.lua
@@ -4,8 +4,11 @@ local text = "¡Hola! Soy Gurky, el de los retos de leveo.\n¿Quieres elegir un 
 
 local function OnGossipHello(event, player, object)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(0, "Alimentar", 0, 1)
-    player:GossipMenuAddItem(0, "Salir", 0, 2)
+    player:GossipMenuAddItem(0, "Reto Hardcore", 0, 1)
+    player:GossipMenuAddItem(0, "Reto Artesano", 0, 2)
+    player:GossipMenuAddItem(0, "Reto Murlocfóbico", 0, 3)
+    player:GossipMenuAddItem(0, "Reto Maestro de Oficios", 0, 4)
+    player:GossipMenuAddItem(0, "Salir", 0, 10)
     player:SendGossipText(text, npc)
     player:GossipSendMenu(npc, object)
 end
