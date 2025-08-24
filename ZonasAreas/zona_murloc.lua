02098507308cbfb506b7ffdf39ff4c67be1ef0ca
Black7in <elperroblack158@gmail.com>
2025-08-24 10:07:12 -0400

    asd

---
+++ ZonasAreas/zona_murloc.lua
@@ -0,0 +1,25 @@
+local AreaId = 616 -- ID de la zona Murloc (Hyjal)
+
+local CMSG_REPOP_REQUEST = 0x15A
+
+local function OnReceiveRepopRequest(event, packet, player)
+    if player:GetAreaId() == AreaId then
+        player:SendNotification("¡Has muerto en la Zona Murloc! Espera que una ValkYria te retorne a la vida.")
+        return false
+    end
+end
+
+
+-- RegisterPacketEvent( CMSG_REPOP_REQUEST, 5, OnReceiveRepopRequest )
+--66
+local function OnTeleportGraveYard(event, player, mapId, x, y, z)
+    if player:GetAreaId() == AreaId then
+        -- Teletransportar al jugador a la zona Murloc
+        player:SendNotification("Has sido teletransportado a la Zona Murloc.")
+    else
+        player:SendNotification("No estás en la Zona Murloc.")
+    end
+
+end
+
+RegisterPlayerEvent(66, OnTeleportGraveYard)
