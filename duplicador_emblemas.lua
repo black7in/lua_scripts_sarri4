cb5effe9c84566dd16c43169dc66ec6c6aa703d3
Black7in <elperroblack158@gmail.com>
2025-08-25 11:29:18 -0400

    Quest doble emblema

--- duplicador_emblemas.lua
+++ duplicador_emblemas.lua
@@ -20,3 +20,22 @@ local function OnLootItem(event, player, item, count)
 end
 
 RegisterPlayerEvent(32, OnLootItem)
+
+
+local function OnQuestRewardItem(event, player, item, count)
+    local itemId = item:GetEntry()
+    local tabardo = Player:GetItemByPos( 255, 18 )
+
+    if tabardo then
+        if tabard:GetEntry() ~= 23388 then
+            return
+        end
+
+        if emblemas[itemId] then
+            local newItemId = emblemas[itemId]
+            player:AddItem(newItemId, count)
+        end
+    end
+end
+
+RegisterPlayerEvent(51, OnQuestRewardItem)
