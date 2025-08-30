6324cea86966c3cc37340f5fd21ebf0d1cd1e223
Black7in <elperroblack158@gmail.com>
2025-08-29 23:58:09 -0400

    asd

--- duplicador_emblemas.lua
+++ duplicador_emblemas.lua
@@ -39,3 +39,21 @@ local function OnQuestRewardItem(event, player, item, count)
 end
 
 -- RegisterPlayerEvent(51, OnQuestRewardItem)
+
+local function OnStoreNewItem(event, player, item, count)
+    local itemId = item:GetEntry()
+    local tabardo = Player:GetItemByPos( 255, 18 )
+
+    if tabardo then
+        if tabard:GetEntry() ~= 23388 then
+            return
+        end
+
+        if emblemas[itemId] then
+            local newItemId = emblemas[itemId]
+            player:AddItem(newItemId, count)
+        end
+    end
+end
+
+RegisterPacketEvent(53, 1, OnStoreNewItem)
