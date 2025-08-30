216b908148f4c304902133ab6b60c1f1e9a4a206
Black7in <elperroblack158@gmail.com>
2025-08-29 21:22:14 -0400

    embed

--- DiscordLogs/gm_login.lua
+++ DiscordLogs/gm_login.lua
@@ -1,7 +1,19 @@
 local function OnLogin(event, player)
     if player:GetGMRank() >= 1 then
-        local content = player:GetName() .. " se ha conectado al servidor."
-        local json = '{"content": "' .. content .. '"}'
+        local playerName = player:GetName()
+        local accountName = player:GetAccountName()
+        
+        local embed = [[
+        {
+          "embeds": [
+            {
+              "title": "📢 Conexión detectada",
+              "description": "La cuenta GM **]] .. accountName .. [[** se ha conectado con el jugador **]] .. playerName .. [[**.",
+              "color": 16711680
+            }
+          ]
+        }
+        ]]
         
         HttpRequest("POST",
             "https://discord.com/api/webhooks/1400275132370653286/VUKbGq7tQ9WMGfb43OAZGTRQaye18marw7Lows9vmns9rA_2SFmFGjfIU8JWXbYsKLG9",
