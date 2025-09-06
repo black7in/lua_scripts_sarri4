2f40a9ebec2ca35d17d4a7c127d07e7544db458e
Black7in <elperroblack158@gmail.com>
2025-09-06 11:02:12 -0400

    asdsa

--- ZonasAreas/zonas_prohibidas.lua
+++ ZonasAreas/zonas_prohibidas.lua
@@ -6,7 +6,7 @@ local zoneId = { 268, 876 }
 
 local function OnEnterZone(event, player, newZone, newArea)
     for i, v in ipairs(zoneId) do
-        if v == newZone and not player:IsGM() then
+        if v == newZone and player:GetGMRank() < 1 then
             player:CastSpell( player, 8690, true )
             player:SendNotification("Zona no disponible para jugadores!")
         end
