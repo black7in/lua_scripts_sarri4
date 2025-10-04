b49ecd3fc3f0e882d9b74b03242d52c03249d5d0
Black7in <elperroblack158@gmail.com>
2025-10-04 13:24:27 -0400

    feat: enhance welcome message with additional information and formatting

--- MensaBienvenida.lua
+++ MensaBienvenida.lua
@@ -1,10 +1,13 @@
 local config = require("config")
 
 local function enviarMensaje(eventid, delay, repeats, player)
-    local msg = "Bienvenido a |cff33FF00"..config.serverName.."|r\n" .. player:GetName() .. "! Disfruta de tu aventura en Azeroth.\n\n" ..
-                "Si necesitas ayuda, puedes contactar a un Game Master o visitar nuestro sitio web.\n" ..
-                "Recuerda que estamos aquí para ayudarte a disfrutar de la mejor experiencia de juego posible.\n\n" ..
-                "¡Diviértete y buena suerte!\n"
+    local msg = "|cff33FF00Bienvenido a |r|cff00CCFF" .. config.serverName .. "|r\n\n" ..
+            "|cffffff00" .. player:GetName() .. "|r, ¡prepárate para tu aventura en Azeroth!\n\n" ..
+            "|cffFF6600Recuerda:|r si necesitas ayuda, contacta a un |cffFF0000Game Master|r o visita nuestro sitio web.\n\n" ..
+            "|cff00FF00Únete al canal global:|r |cffffff00/join global|r\n" ..
+            "|cff00CCFFTambién puedes entrar a nuestro Discord para más información y comunidad:|r |cffffff00[link de Discord]|r\n\n" ..
+            "|cffFF33CC¡Diviértete, haz amigos y buena suerte en tus batallas!|r\n"
+
     player:GossipClearMenu()
     player:GossipMenuAddItem(1, "", 0, 1, false, msg)
     player:GossipSendMenu(1, player, 0)
