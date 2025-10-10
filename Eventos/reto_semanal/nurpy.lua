a0c5ad01173001c99c3304964dbda0f2cd6ad075
Black7in <elperroblack158@gmail.com>
2025-10-09 23:47:46 -0400

    feat: add time frame for Nurpy weekly challenge participation

--- Eventos/reto_semanal/nurpy.lua
+++ Eventos/reto_semanal/nurpy.lua
@@ -2,6 +2,8 @@ local npc = 50020
 
 local texto = "Saludos, aventurero. ¿Quieres participar en el reto semanal de Nurpy?\nPara ganar, solo necesitas cumplir con algunos objetivos:\n1. Acumular 20 horas de juego.\n2. Superar el nivel 50.\n3. Recolecta 10 objetos perdidos de Nurpy.\n\nSi completas estos objetivos, recibirás una recompensa especial:\n1. |TINTERFACE/ICONS/inv_egg_03:15:15:0:0|t Huevo de Lurky\n2. |TINTERFACE/ICONS/spell_holy_summonchampion:15:15:0:0|t Emblemas de Triunfo x50.\n\n¿Estás listo para el desafío?"
 
+texto = texto .. "\n\nImportante: El reto comienza desde el viernes a las 6:00 am  hasta el lunes a las 6:00 am. Asegúrate de completar los objetivos y recoger tu recompensa dentro de estas fechas."
+
 local function OnGossipHello(event, player, creature)
     player:GossipClearMenu()
     player:GossipMenuAddItem(0, "Ver mi progreso.", 0, 1)
